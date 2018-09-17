0688270333e1dbe1e2471cdfec63e9a47f9bcf87
Eliseu Daroit <eliseudaroitjr@gmail.com>
2018-09-16 21:10:55 -0300

    [#13] Fix rails credentials

--- server/app/lib/json_web_token.rb
+++ server/app/lib/json_web_token.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class JsonWebToken
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
-
+  HMAC_SECRET = Rails.application.credentials.secret_key_base
+  
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, HMAC_SECRET)
